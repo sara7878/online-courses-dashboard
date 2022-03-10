e849fbd494507aead8ea9712c01eaa8cf48a1ee3
Mahmoudawd4 <mahmoudawd54@gmail.com>
2022-03-10 13:25:48 +0200

    prod larava

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
 
-  baseUrl: 'https://shortonlinecourses.herokuapp.com/',
+  baseUrl: 'https://short-online-courses.herokuapp.com/',
   publishableKeyStripe:'pk_test_51KY7OyGurDTsIveDgdUzsTOYnK2M2yuGCIsqpaSjoQVRYaIgGoSSghxfGmJHZxTH30EkMikkhZA18REaBuCHEuQE00kkYon143'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,6 +2,6 @@
 export const environment = {
   production: false,
 
-  baseUrl: 'https://shortonlinecourses.herokuapp.com/',
+  baseUrl: 'https://short-online-courses.herokuapp.com/',
   publishableKeyStripe:'pk_test_51KY7OyGurDTsIveDgdUzsTOYnK2M2yuGCIsqpaSjoQVRYaIgGoSSghxfGmJHZxTH30EkMikkhZA18REaBuCHEuQE00kkYon143'
 };
